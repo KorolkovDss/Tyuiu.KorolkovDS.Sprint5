30ee31c071832225fa7381fab864ef188fcc189a
Дмитрий Корольков <Tyuiu.KorolkovDS@outlook.com>
2025-11-24 21:59:45 +0500

    Task4

--- Tyuiu.KorolkovDS.Sprint5.Task4.V3.Lib/DataService.cs
+++ Tyuiu.KorolkovDS.Sprint5.Task4.V3.Lib/DataService.cs
@@ -7,7 +7,7 @@ namespace Tyuiu.KorolkovDS.Sprint5.Task4.V3.Lib
         public double LoadFromDataFile(string path)
         {
             double x = Convert.ToDouble (File.ReadAllText(path).Replace(".",","));
-            double res = Math.Round((Math.Sin(x)+4)/x ,3);
+            double res = Math.Round(((Math.Sin(x)+4)/x) - 1.25 * x ,3);
             return res ;            
         }
     }
