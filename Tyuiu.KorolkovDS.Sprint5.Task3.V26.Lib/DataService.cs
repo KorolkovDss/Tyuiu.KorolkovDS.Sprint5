d5c077058fc7f6c82202579391f2469835054ec6
Дмитрий Корольков <Tyuiu.KorolkovDS@outlook.com>
2025-11-24 21:08:59 +0500

    Task3

---
+++ Tyuiu.KorolkovDS.Sprint5.Task3.V26.Lib/DataService.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using System.Text;
+
+
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.KorolkovDS.Sprint5.Task3.V26.Lib
+{
+    public class DataService : ISprint5Task3V26
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string path = $@"{Path.GetTempPath()}OutPutFileTask3.bin";
+            double y = Math.Round(0.7 * Math.Pow(x,3) + 1.52 * Math.Pow(x,2),3);
+            using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.Create),Encoding.UTF8))
+            {
+                writer.Write(BitConverter.GetBytes(y));
+            }
+            return path; 
+        }
+    }
+}
---
+++ Tyuiu.KorolkovDS.Sprint5.Task3.V26/Program.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.KorolkovDS.Sprint5.Task3.V26.Lib;
+DataService ds = new DataService();
+int x = 2;
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("X = " +  x);
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+string res = ds.SaveToFileTextData(x);
+
+Console.WriteLine("Файл: " + res);
+Console.WriteLine("Создан!");
+Console.ReadKey();
