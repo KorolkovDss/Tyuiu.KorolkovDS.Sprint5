2c1c9c2125c638c246f86604c3ae00c170dc6b55
Дмитрий Корольков <Tyuiu.KorolkovDS@outlook.com>
2025-11-24 20:05:21 +0500

    Task2

---
+++ Tyuiu.KorolkovDS.Sprint5.Task2.V2.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.KorolkovDS.Sprint5.Task2.V2.Lib;
+
+namespace Tyuiu.KorolkovDS.Sprint5.Task2.V2.Test
+{
+    [TestClass]
+    public sealed class DataServiceTEST
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = Path.GetTempFileName();
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+    
+}
---
+++ Tyuiu.KorolkovDS.Sprint5.Task2.V2/Program.cs
@@ -0,0 +1,29 @@
+﻿using Tyuiu.KorolkovDS.Sprint5.Task2.V2.Lib;
+DataService ds  = new DataService();
+int[,] matrix = new int[3, 3] { { -7, 7, 5 }, 
+                                { 4, 2, -7 }, 
+                                { 2, 6, -4 } };
+int rows = matrix.GetUpperBound(0) + 1;
+int cols = matrix.Length / rows;
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("Массив: ");
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < cols; j++)
+    {
+        Console.Write($"{matrix[i, j]}\t");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+string res = ds.SaveToFileTextData(matrix);
+
+Console.WriteLine("Файл: " + res);
+Console.WriteLine("Создан!");
+Console.ReadKey();
