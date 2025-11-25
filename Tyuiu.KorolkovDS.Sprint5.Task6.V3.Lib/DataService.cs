3959dc845d6fc04739c2191020381c8288019877
Дмитрий Корольков <Tyuiu.KorolkovDS@outlook.com>
2025-11-25 23:09:11 +0500

    Task6

---
+++ Tyuiu.KorolkovDS.Sprint5.Task6.V3.Lib/DataService.cs
@@ -0,0 +1,23 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5; 
+using System.IO;
+
+namespace Tyuiu.KorolkovDS.Sprint5.Task6.V3.Lib
+{
+    public class DataService : ISprint5Task6V3
+    {
+        public int LoadFromDataFile(string path)
+        {
+            string str = File.ReadAllText(path);
+            int count = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] >= 'a' && str[i] <= 'z' || str[i] >= 'A' && str[i] <= 'Z')
+                {
+                    count++;
+
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ Tyuiu.KorolkovDS.Sprint5.Task6.V3/Program.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.KorolkovDS.Sprint5.Task6.V3.Lib;
+DataService ds = new DataService();
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+string path = $@"C:\DataSprint5\InPutDataFileTask6V3.txt";
+double res = ds.LoadFromDataFile(path);
+
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine(res);
+
+Console.ReadKey();
