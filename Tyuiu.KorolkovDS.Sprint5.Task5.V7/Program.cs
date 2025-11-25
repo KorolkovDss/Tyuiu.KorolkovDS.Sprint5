b5ea2b5fc96b95533979043e11943494850c4d8b
Дмитрий Корольков <Tyuiu.KorolkovDS@outlook.com>
2025-11-25 22:25:12 +0500

    Task5

---
+++ Tyuiu.KorolkovDS.Sprint5.Task5.V7/Program.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.KorolkovDS.Sprint5.Task5.V7.Lib;
+DataService ds = new DataService();
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+string path = Path.GetFileName("InPutDataFileTask5V7.txt");
+Console.WriteLine("Данные находятся в файле: " + path);
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+double res = ds.LoadFromDataFile(path);
+Console.WriteLine(res);
+Console.ReadKey();
