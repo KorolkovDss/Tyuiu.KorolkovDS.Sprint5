65b00905213ef380a205a9e2213ebc1ff8d12422
Дмитрий Корольков <Tyuiu.KorolkovDS@outlook.com>
2025-11-25 23:27:25 +0500

    Task7

---
+++ Tyuiu.KorolkovDS.Sprint5.Task7.V14.Lib/DataService.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.KorolkovDS.Sprint5.Task7.V14.Lib
+{
+    public class DataService : ISprint5Task7V14
+    {
+        public string LoadDataAndSave(string path)
+        {
+            string pathSaveFile = $@"{Directory.GetCurrentDirectory()}\OutPutDataFileTask7V14.txt";
+
+            FileInfo fileInfo = new FileInfo(pathSaveFile);
+            bool fileExists = fileInfo.Exists;
+
+            if (fileExists)
+            {
+                File.Delete(pathSaveFile);
+            }
+
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    line = line.ToUpper();
+
+                    File.AppendAllText(pathSaveFile, line + Environment.NewLine);
+
+
+                }
+            }
+            return pathSaveFile;
+        }
+    }
+}
---
+++ Tyuiu.KorolkovDS.Sprint5.Task7.V14.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿namespace Tyuiu.KorolkovDS.Sprint5.Task7.V14.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = $@"C:\DataSprint5\InPutDataFileTask7V14.txt";
+            FileInfo fileinfo = new FileInfo(path);
+            bool fileExists = fileinfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
